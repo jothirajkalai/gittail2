cf742abfdcb06093487b05e19dfed059ccb6e5ad
jothirajkalai <jothirajkalai95@gmail.com>
2022-11-23 11:53:38 +0530

    some line of code will be added

---
+++ src/org/gittrail/Git.java
@@ -0,0 +1,14 @@
+package org.gittrail;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=14;
+		
+		System.out.println(a);
+
+	}
+
+}
